792c6733593e056cdd79a2a77b4e4feaec608c4d
WMXPY <wm@mengw.io>
2020-02-11 12:09:20 -0600

    remove example

--- example/cluster.ts
+++ example/cluster.ts
@@ -5,17 +5,11 @@
  */
 
 import { RedisManager } from "../src/manager";
-import * as IORedis from "ioredis";
 
-const a = new IORedis({
+const manager: RedisManager = RedisManager.fromNodes({
     port: 6379,
     host: 'localhost',
+}, {
+    port: 3280,
+    host: 'localhost',
 });
-
-// const manager: RedisManager = RedisManager.fromNodes({
-//     port: 6379,
-//     host: 'localhost',
-// }, {
-//     port: 3280,
-//     host: 'localhost',
-// });
